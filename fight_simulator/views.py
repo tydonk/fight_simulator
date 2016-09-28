4b760a00d3e9a80acca493ac248f96ade84b3652
Tydon Kommel <tydonk@Tydon-Kommels-MacBook-Pro.local>
2016-09-27 19:01:27 -0700

    working on jquery men filtering. added local css and js files for working offline

--- fight_simulator/views.py
+++ fight_simulator/views.py
@@ -6,32 +6,6 @@ from flask_login import login_required, login_user, current_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from jsonschema import validate, ValidationError
 
-
-fighter_schema = {
-    "properties":{
-        "first_name": {"type" : "string"},
-        "last_name": {"type" : "string"},
-        "nickname": {"type" : "string"},
-        "gender": {"type" : "string"},
-        "promotion": {"type" : "string"},
-        "fighter_image": {"type" : "string"},
-        "weight": {"type" : "string"},
-        "win": {"type" : "number"},
-        "loss": {"type" : "number"},
-        "draw": {"type" : "number"},
-        },
-        "required": [
-                    "first_name",
-                    "last_name",
-                    "gender",
-                    "promotion"
-                    "weight",
-                    "win",
-                    "loss",
-                    "draw",
-                    ]
-}
-
 @app.route("/", methods=["GET"])
 def welcome():
     return render_template("welcome.html")
@@ -41,41 +15,8 @@ def welcome():
 @decorators.accept("application/json")
 def fight():
     fighter_data = session.query(Fighter)
-    return render_template("fight.html", 
-        data=json.dumps([fighter.as_dictionary() for fighter in fighter_data]))
-    '''fighter_data = session.query(Fighter)
-    fighter_data = fighter_data.order_by(Fighter.last_name.asc())
-    for fighter in fighter_data:
-        first_name = fighter.first_name
-        last_name = fighter.last_name
-        win = fighter.win
-        loss = fighter.loss
-        draw = fighter.draw
-        weight = fighter.weight       
-    return render_template("fight.html", 
-                    data=fighter_data, 
-                    first_name=first_name, 
-                    last_name=last_name,
-                    win = win,
-                    loss = loss,
-                    draw = draw,
-                    weight = weight,
-                    )'''
-
-@app.route("/fight", methods=["GET", "POST"])
-def selected_gender():
-    if request.form["red_gender"] == "Female":
-        female_fighters = session.query(Fighter).filter(Fighter.gender == "female").all()
-        return render_template("fight.html", 
-            data = json.dumps([fighter.as_dictionary() for fighter in female_fighters]))
-
-@app.route("/fight/promotion", methods=["POST"])
-def selected_promotion():
-    if request.form["red_promo"] == "UFC":
-        ufc_fighters = session.query(Fighter).filter(Fighter.promotion == "UFC").all()
-        data = json.dumps([fighter.as_dictionary()
-                            for fighter in ufc_fighters])
-
+    return Response(render_template("fight.html", 
+                    data=fighter_data, mimetype="application/json"))
 
 @app.route("/login", methods=["GET"])
 def login_get():
@@ -101,6 +42,7 @@ def create_user_get():
 def create_user_post():
     email = request.form["email"]
     if session.query(User).filter_by(email=email).first():
+        flash("This email address is already in use, please choose another", "danger")
         return redirect(url_for("create_user_get"))
     
     password = request.form["password"]
@@ -111,6 +53,7 @@ def create_user_post():
         session.commit()
         return redirect(url_for("login_get"))
     else:
+        flash("Password must be at least 8 characters", "danger")
         return    
          
 @app.route("/logout")
@@ -118,3 +61,28 @@ def create_user_post():
 def logout():
     logout_user()
     return redirect(url_for("welcome"))
+
+fighter_schema = {
+    "properties":{
+        "first_name": {"type" : "string"},
+        "last_name": {"type" : "string"},
+        "nickname": {"type" : "string"},
+        "gender": {"type" : "string"},
+        "promotion": {"type" : "string"},
+        "fighter_image": {"type" : "string"},
+        "weight": {"type" : "string"},
+        "win": {"type" : "number"},
+        "loss": {"type" : "number"},
+        "draw": {"type" : "number"},
+        },
+        "required": [
+                    "first_name",
+                    "last_name",
+                    "gender",
+                    "promotion"
+                    "weight",
+                    "win",
+                    "loss",
+                    "draw",
+                    ]
+}
