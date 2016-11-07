7fec30f58f524396a55797aedd19205fea124499
Tydon Kommel <tydonk@Tydon-Kommels-MacBook-Pro.local>
2016-11-07 11:11:59 -0800

    removed 'results' field and added 'as_dictionary' function to return results for user history

--- fight_simulator/database.py
+++ fight_simulator/database.py
@@ -40,32 +40,47 @@ class Fighter(Base):
             "weight": self.weight,
             "win": self.win,
             "loss": self.loss,
-            "draw": self.draw,    
+            "draw": self.draw,
         }
         return fighter
 
 class User(Base, UserMixin):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(1024), unique=True, nullable=False)
     password = Column(String(128), nullable=False)
 
     user_history = relationship("History", backref="user")
-    
+
 class History(Base):
     __tablename__ = "history"
-    
+
     id = Column(Integer, primary_key=True)
     fight_date = Column(DateTime, nullable=False)
     has_occured = Column(Boolean, nullable=False)
     red_corner = Column(String(1024), nullable=False)
     blue_corner = Column(String(1024), nullable=False)
     winner = Column(String(1024))
-    result = Column(String(1024), nullable=False)
     end_round = Column(Integer, nullable=False)
     end_time = Column(Integer, nullable=False)
+    method = Column(String, nullable=False)
 
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    def as_dictionary(self):
+        results = {
+            "id": self.id,
+            "fight_date": self.fight_date,
+            "has_occured": self.has_occured,
+            "red_corner": self.red_corner,
+            "blue_corner": self.blue_corner,
+            "winnner": self.winner,
+            "result": self.result,
+            "end_round": self.end_round,
+            "end_time": self.end_time,
+            "method": self.method,
+        }
+        return results
 
 Base.metadata.create_all(engine)
