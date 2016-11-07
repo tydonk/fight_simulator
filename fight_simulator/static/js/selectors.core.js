3df26c8a19b58817ba2fb61b72d7dd6943d09f2a
Tydon Kommel <tydonk@Tydon-Kommels-MacBook-Pro.local>
2016-11-07 11:16:02 -0800

    deactivated validate fighters function and moved it to server side. update 'ui_load_fighter_info' function to populate dropdowns with fighter attributes

--- fight_simulator/static/js/selectors.core.js
+++ fight_simulator/static/js/selectors.core.js
@@ -438,8 +438,30 @@ function ui_load_fighter_info(corner, name) {
   }
 }
 
+// check to make sure the fighters are not the same
+/* function validate_fight(red_fighter, blue_fighter) {
+  var red_menu_sel = $('.corner')[0].getElementsByTagName('select');
+	var red_fighter_val = $(red_menu_sel[3]).selectpicker('val');
+  var blue_menu_sel = $('.corner')[1].getElementsByTagName('select');
+	var blue_fighter_val = $(blue_menu_sel[3]).selectpicker('val');
+  var red_fighter = get_fighters_by_name(red_fighter_val);
+  var blue_fighter = get_fighters_by_name(blue_fighter_val);
+
+  $('#fight').on('click', function() {
+    if (validate_pairs(red_fighter[0].id, blue_fighter[0].id)) {
+      $('#illegal_alert').show();
+      console.log("fight is illegal");
+      return;
+    }
+    console.log("fight permitted");
+    $('#illegal_alert').hide();
+  });
+} */
+
 $(document).ready(function() {
+  $('#illegal_alert').hide();
   $('.corner').change(corner_event_handler);
+  //validate_fight();
   reset_all_menus();
   reset_corner();
 });
