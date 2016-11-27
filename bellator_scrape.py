1a88c89e9827dccb7cec52da45ef182d5fca2ae4
Tydon Kommel <tydonk@Tydon-Kommels-MacBook-Pro.local>
2016-11-26 23:10:42 -0800

    scraper works, now adds Bellator fighters to DB. no fighter images at the moment

--- bellator_scrape.py
+++ bellator_scrape.py
@@ -1,17 +1,51 @@
 import requests
 import manage
+import untangle
 from manage import session
 from fight_simulator import database, config, __init__, views
-from fight_simulator.database import User, Fighter, History
+from fight_simulator.database import Fighter
 
-import urllib
-from urllib.request import urlopen
-import xml.etree.ElementTree as etree
+xml = "http://api.spike.com/feeds/bellator/1.0/fighters?key=BELLATORAPPKEY&numberOfItems=188&pageNumber=1"
 
-response = urllib.request.urlopen("http://api.spike.com/feeds/bellator/1.0/fighters?key=BELLATORAPPKEY&numberOfItems=200&pageNumber=1")
-tree = etree.parse(response)
-for each in tree:
-    print(each)
-root = tree.getroot()
-for branch in root:
-    print(branch)
+bellator_f = untangle.parse(xml)
+fighters = bellator_f.fighters.fighter
+
+for fighter in fighters:
+    if fighter.firstName != "To Be Announced":
+        record = fighter.record.cdata
+        win = record.split('-')[0]
+        loss = record.split('-')[1]
+        draw = record.split('-')[2]
+        if fighter.sex.cdata == 'W':
+            gender = 'female'
+        else:
+            gender = 'male'
+        fweight = float(fighter.weight.cdata)
+        if fweight <= 126:
+            weight = "Flyweight"
+        if 127 <= fweight <= 136:
+            weight = "Bantamweight"
+        if 137 <= fweight <= 146:
+            weight = "Featherweight"
+        if 147 <= fweight <= 156:
+            weight = "Lightweight"
+        if 157 <= fweight <= 171:
+            weight = "Welterweight"
+        if 172 <= fweight <= 186:
+            weight = "Middleweight"
+        if 187 <= fweight <= 206:
+            weight = "Light Heavyweight"
+        if 207 <= fweight:
+            weight = "Heavyweight"
+        fighter = Fighter(
+            first_name = fighter.firstName.cdata,
+            last_name = fighter.lastName.cdata,
+            gender = gender,
+            promotion = "Bellator",
+            weight = weight,
+            win = win,
+            loss = loss,
+            draw = draw,
+            )
+        session.add(fighter)
+session.commit()
