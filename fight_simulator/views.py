ccee0a5961805123ad4aaf631850c735ce1f3a0e
Tydon Kommel <tydonk@Tydon-Kommels-MacBook-Pro.local>
2017-01-01 17:07:43 -0800

    modified home endpoint

--- fight_simulator/views.py
+++ fight_simulator/views.py
@@ -10,7 +10,7 @@ from random import randint
 import random
 from .login import login_manager
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def welcome():
     return render_template("welcome.html")
 
