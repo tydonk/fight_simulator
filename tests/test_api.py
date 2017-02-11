14a8cad3578a1b823d560da0df01d4dfc4fae5fb
Tydon Kommel <tydonk@Tydons-MacBook-Pro.local>
2017-02-10 22:54:28 -0800

    formatted for PEP 8 compliance

--- tests/test_api.py
+++ tests/test_api.py
@@ -33,10 +33,6 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(data["message"],
                          "Request must accept application/json data")
 
-    def test_unsupported_mimetype(self):
-        data = "<xml></xml>"
-        response = self.client.post("/api/fighters")
-
     def test_get_empty_fighters(self):
         """ Get fighters from empty database """
         response = self.client.get("/api/fighters",
@@ -52,32 +48,32 @@ class TestAPI(unittest.TestCase):
     def test_get_fighters(self):
         """ Get all fighters from populated database """
         fighterA = Fighter(
-                        first_name='Jon',
-                        last_name='Jones',
-                        gender='male',
-                        promotion='UFC',
-                        weight='Light Heavyweight',
-                        win=20,
-                        loss=1,
-                        )
+            first_name='Jon',
+            last_name='Jones',
+            gender='male',
+            promotion='UFC',
+            weight='Light Heavyweight',
+            win=20,
+            loss=1,
+        )
         fighterB = Fighter(
-                        first_name='Conor',
-                        last_name='McGregor',
-                        gender='male',
-                        promotion='UFC',
-                        weight='Lightweight',
-                        win=22,
-                        loss=3,
-                        )
+            first_name='Conor',
+            last_name='McGregor',
+            gender='male',
+            promotion='UFC',
+            weight='Lightweight',
+            win=22,
+            loss=3,
+        )
         fighterC = Fighter(
-                        first_name='Amanda',
-                        last_name='Nunes',
-                        gender='female',
-                        promotion='UFC',
-                        weight='Bantamweight',
-                        win=14,
-                        loss=4,
-                        )
+            first_name='Amanda',
+            last_name='Nunes',
+            gender='female',
+            promotion='UFC',
+            weight='Bantamweight',
+            win=14,
+            loss=4,
+        )
         session.add_all([fighterA, fighterB, fighterC])
         session.commit()
 
@@ -91,26 +87,38 @@ class TestAPI(unittest.TestCase):
         fighter = json.loads(response.data.decode("ascii"))
         self.assertEqual(len(fighter), 3)
 
+    def test_get_nonexistent_fighter(self):
+        """ Attempt to get fighter that does not exists """
+        response = self.client.get("/api/fighter/1/",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data.decode("ascii"))
+        self.assertEqual(data["message"], "Could not find fighter with id 1")
+
     def test_get_fighter_by_id(self):
         """ Get single fighter by their id """
         fighterA = Fighter(
-                        first_name='Jon',
-                        last_name='Jones',
-                        gender='male',
-                        promotion='UFC',
-                        weight='Light Heavyweight',
-                        win=20,
-                        loss=1,
-                        )
+            first_name='Jon',
+            last_name='Jones',
+            gender='male',
+            promotion='UFC',
+            weight='Light Heavyweight',
+            win=20,
+            loss=1,
+        )
         fighterB = Fighter(
-                        first_name='Conor',
-                        last_name='McGregor',
-                        gender='male',
-                        promotion='UFC',
-                        weight='Lightweight',
-                        win=22,
-                        loss=3,
-                        )
+            first_name='Conor',
+            last_name='McGregor',
+            gender='male',
+            promotion='UFC',
+            weight='Lightweight',
+            win=22,
+            loss=3,
+        )
         session.add_all([fighterA, fighterB])
         session.commit()
 
@@ -120,11 +128,8 @@ class TestAPI(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, "application/json")
-
         fighter = json.loads(response.data.decode("ascii"))
-        self.assertEqual(len(fighter), 1)
-
-        fighter = fighter[0]
+        self.assertEqual(len(fighter), 16)
         self.assertEqual(fighter['id'], 2)
         self.assertEqual(fighter['first_name'], 'Conor')
         self.assertEqual(fighter['last_name'], 'McGregor')
@@ -132,35 +137,35 @@ class TestAPI(unittest.TestCase):
     def test_get_fighter_by_name(self):
         """ Get single fighter by their name """
         fighterA = Fighter(
-                        first_name='Jon',
-                        last_name='Jones',
-                        gender='male',
-                        promotion='UFC',
-                        weight='Light Heavyweight',
-                        win=20,
-                        loss=1,
-                        )
+            first_name='Jon',
+            last_name='Jones',
+            gender='male',
+            promotion='UFC',
+            weight='Light Heavyweight',
+            win=20,
+            loss=1,
+        )
         fighterB = Fighter(
-                        first_name='Amanda',
-                        last_name='Nunes',
-                        gender='female',
-                        promotion='UFC',
-                        weight='Bantamweight',
-                        win=14,
-                        loss=4,
-                        )
+            first_name='Amanda',
+            last_name='Nunes',
+            gender='female',
+            promotion='UFC',
+            weight='Bantamweight',
+            win=14,
+            loss=4,
+        )
         session.add_all([fighterA, fighterB])
         session.commit()
 
-        response = self.client.get(
-            "/api/fighter/name/{}/{}/".format(
-            fighterB.last_name, fighterB.first_name),
+        response = self.client.get("/api/fighter/name/{}/{}/".format(
+                fighterB.last_name,
+                fighterB.first_name
+            ),
             headers=[("Accept", "application/json")]
         )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, "application/json")
-
         fighter = json.loads(response.data.decode("ascii"))
         self.assertEqual(len(fighter), 1)
 
@@ -171,32 +176,32 @@ class TestAPI(unittest.TestCase):
     def test_get_fighters_by_gender(self):
         """ Get all fighters with a specific gender """
         fighterA = Fighter(
-                        first_name='Jon',
-                        last_name='Jones',
-                        gender='male',
-                        promotion='UFC',
-                        weight='Light Heavyweight',
-                        win=20,
-                        loss=1,
-                        )
+            first_name='Jon',
+            last_name='Jones',
+            gender='male',
+            promotion='UFC',
+            weight='Light Heavyweight',
+            win=20,
+            loss=1,
+        )
         fighterB = Fighter(
-                        first_name='Conor',
-                        last_name='McGregor',
-                        gender='male',
-                        promotion='UFC',
-                        weight='Lightweight',
-                        win=22,
-                        loss=3,
-                        )
+            first_name='Conor',
+            last_name='McGregor',
+            gender='male',
+            promotion='UFC',
+            weight='Lightweight',
+            win=22,
+            loss=3,
+        )
         fighterC = Fighter(
-                        first_name='Amanda',
-                        last_name='Nunes',
-                        gender='female',
-                        promotion='UFC',
-                        weight='Bantamweight',
-                        win=14,
-                        loss=4,
-                        )
+            first_name='Amanda',
+            last_name='Nunes',
+            gender='female',
+            promotion='UFC',
+            weight='Bantamweight',
+            win=14,
+            loss=4,
+        )
         session.add_all([fighterA, fighterB, fighterC])
         session.commit()
 
@@ -222,32 +227,32 @@ class TestAPI(unittest.TestCase):
     def test_get_fighters_by_gender_promotion(self):
         """ Get all fighters with a specific gender and promotion """
         fighterA = Fighter(
-                        first_name='Jon',
-                        last_name='Jones',
-                        gender='male',
-                        promotion='UFC',
-                        weight='Light Heavyweight',
-                        win=20,
-                        loss=1,
-                        )
+            first_name='Jon',
+            last_name='Jones',
+            gender='male',
+            promotion='UFC',
+            weight='Light Heavyweight',
+            win=20,
+            loss=1,
+        )
         fighterB = Fighter(
-                        first_name='Michael',
-                        last_name='Chandler',
-                        gender='male',
-                        promotion='Bellator',
-                        weight='Lightweight',
-                        win=22,
-                        loss=3,
-                        )
+            first_name='Michael',
+            last_name='Chandler',
+            gender='male',
+            promotion='Bellator',
+            weight='Lightweight',
+            win=22,
+            loss=3,
+        )
         fighterC = Fighter(
-                        first_name='Amanda',
-                        last_name='Nunes',
-                        gender='female',
-                        promotion='UFC',
-                        weight='Bantamweight',
-                        win=14,
-                        loss=4,
-                        )
+            first_name='Amanda',
+            last_name='Nunes',
+            gender='female',
+            promotion='UFC',
+            weight='Bantamweight',
+            win=14,
+            loss=4,
+        )
         session.add_all([fighterA, fighterB, fighterC])
         session.commit()
 
@@ -271,41 +276,37 @@ class TestAPI(unittest.TestCase):
         Get all fighters with a specific gender, promotion and weight class
         """
         fighterA = Fighter(
-                        first_name='Jon',
-                        last_name='Jones',
-                        gender='male',
-                        promotion='UFC',
-                        weight='Light Heavyweight',
-                        win=20,
-                        loss=1,
-                        )
+            first_name='Jon',
+            last_name='Jones',
+            gender='male',
+            promotion='UFC',
+            weight='Light Heavyweight',
+            win=20,
+            loss=1,
+        )
         fighterB = Fighter(
-                        first_name='Michael',
-                        last_name='Chandler',
-                        gender='male',
-                        promotion='Bellator',
-                        weight='Lightweight',
-                        win=22,
-                        loss=3,
-                        )
+            first_name='Michael',
+            last_name='Chandler',
+            gender='male',
+            promotion='Bellator',
+            weight='Lightweight',
+            win=22,
+            loss=3,
+        )
         fighterC = Fighter(
-                        first_name='Amanda',
-                        last_name='Nunes',
-                        gender='female',
-                        promotion='UFC',
-                        weight='Bantamweight',
-                        win=14,
-                        loss=4,
-                        )
+            first_name='Amanda',
+            last_name='Nunes',
+            gender='female',
+            promotion='UFC',
+            weight='Bantamweight',
+            win=14,
+            loss=4,
+        )
         session.add_all([fighterA, fighterB, fighterC])
         session.commit()
 
-        response = self.client.get(
-            "/api/fighters/{}/{}/{}/".format(
-                                            fighterB.gender,
-                                            fighterB.promotion,
-                                            fighterB.weight
-                                            ),
+        response = self.client.get("/api/fighters/{}/{}/{}/".format(
+            fighterB.gender, fighterB.promotion, fighterB.weight),
             headers=[("Accept", "application/json")]
         )
 
